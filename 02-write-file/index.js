4e3115c112bd7630e671618ca88e6e45028f8b60
Igor Pavetka <uropb.86@gmail.com>
2022-05-23 14:22:08 +0300

    fix: task 2 --> add check on exit with uppercase on word exit and on space before and after word

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,23 +5,23 @@ const currentPath = path.join(__dirname, 'text.txt');
 const { stdin } = process;
 
 process.stdin.resume();
-console.log('hello, you can write something here, and I add it to text.txt');
+console.log('_________________\nHello, you can type something here, and I add it to text.txt');
+console.log('---> ** for exit you can press Ctrl+C or type "exit"');
 const output = fs.createWriteStream(currentPath, 'utf-8');
 
 output.on('error', error => console.log('Something`re going wrong, look at this error', error.message));
 stdin.on('error', error => console.log('Something is going wrong, look at this error', error.message));
 
 process.on('SIGINT', () => {
-  console.log('Thank you! Goodbye');
+  console.log('_________________\nThank you! Goodbye');
   process.exit();
 });
 
 stdin.on('data', data => {
-  if (data.toString().trimEnd() !== 'exit') {
+  if (data.toString().trim().toUpperCase() !== 'EXIT') {
     output.write(data.toString());
-
   } else {
-    console.log('Thank you! Goodbye');
+    console.log('_________________\nThank you! Goodbye');
     process.exit();
   }
 });
