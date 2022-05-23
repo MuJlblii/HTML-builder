ac3711d24e7960ae08b81b135d350eef9df24137
Igor Pavetka <uropb.86@gmail.com>
2022-05-23 14:48:17 +0300

    feat: add subfolder copy for 4-task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,26 +4,29 @@ const path = require('path');
 const filesCopyFolder = path.join(__dirname,'files-copy');
 const targetCopyFolder = path.join(__dirname,'files');
 
-async function copy() {
-  await fs.promises.rm(filesCopyFolder, {recursive: true, force: true});
-  await fs.promises.mkdir(filesCopyFolder, {recursive: true});
-  fs.readdir(targetCopyFolder,
-    { withFileTypes: true },
-    (err, files) => {
-      if (err)
-        console.log(err);
-      else {
-        files.forEach(file => {
-          if (file.isFile()) {
-            (async () => {
-              await fs.promises.copyFile(path.join(targetCopyFolder,file.name),path.join(filesCopyFolder,file.name));
-            })().catch(console.error);
+async function copy(CopyFromFolder, CopyToFolder) {
+    await fs.promises.mkdir(CopyToFolder, { recursive: true });
+    fs.readdir(CopyFromFolder,
+      { withFileTypes: true },
+      async (err, files) => {
+        if (err)
+          console.log(err);
+        else {
+          for (const file of files) {
+            if (file.isFile()) {
+              await fs.promises.copyFile(path.join(CopyFromFolder, file.name), path.join(CopyToFolder, file.name));
+            } else {
+              await copy(path.join(CopyFromFolder, file.name), path.join(CopyToFolder, file.name));
+            }
           }
-                    
-        });
-        console.log('copied folder');
-      }
-    });
-} 
+        }
+      });
+  }
 
-copy();
+  (async () => {
+    console.log('\nStart copy folder -->');
+    await fs.promises.rm(filesCopyFolder, { recursive: true, force: true });
+    await fs.promises.mkdir(filesCopyFolder, { recursive: true });
+    await copy(targetCopyFolder, filesCopyFolder);
+    await console.log('--> Succesfully finished copying folder\n');
+  })();
